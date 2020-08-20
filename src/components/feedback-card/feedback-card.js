9ad4c0634c6fcf5e4b2199561af71af5881c95e6
Radu Stoica <radu.stoica1994@gmail.com>
2020-08-20 10:43:37 +0200

    add question

---
+++ src/components/feedback-card/feedback-card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./feedback-card.css";
+
+export default function Card() {
+  return (
+    <div className="wrapper-card">
+      <p className="question-card">
+        How likely are you to answer to this question?
+      </p>
+    </div>
+  );
+}
